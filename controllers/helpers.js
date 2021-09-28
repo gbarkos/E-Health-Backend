4919e21b75177135e03d03af3d6137d97dbcb297
vasberc <vasberc@gmail.com>
2021-09-28 14:41:28 +0300

    let and const

--- controllers/helpers.js
+++ controllers/helpers.js
@@ -12,7 +12,7 @@ module.exports.findaDoctor = async () => {
 }
 
 module.exports.randomBloodType = () => {
-    let bloodTypes = ["A+","A-","B+","B-","0+","0-","AB+","AB-"];
+    const bloodTypes = ["A+","A-","B+","B-","0+","0-","AB+","AB-"];
     return bloodTypes[Math.floor(Math.random()*bloodTypes.length-1)]; 
 }
 
