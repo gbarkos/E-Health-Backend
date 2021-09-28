7d66988b77b033048e6b12fc1a262490c97370fc
vasberc <vasberc@gmail.com>
2021-09-28 12:59:42 +0300

    fix status code to 201 created

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,7 +16,7 @@ exports.createUser = async (req, res) => {
     try{
         const newUser = await User.create(req.body);
 
-        res.status(200).json({
+        res.status(201).json({
             status : 'success',
             data: {
                 user: newUser
