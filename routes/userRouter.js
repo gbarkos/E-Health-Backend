5a0eeb0b9daf7baa92687e54b8f571e5154b6e34
vasberc <vasberc@gmail.com>
2021-09-28 11:48:48 +0300

    test post

---
+++ routes/userRouter.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const userController = require('./../controllers/userController');
+
+const router = express.Router();
+
+router
+    .route("/")
+    .post((req, res) => {
+        res.statusCode(201).json({"status": "success"});
+    });
+    // .get(userController.getAllUsers)
+    // .post(userController.createUser);
+
+router
+    .route("/:id")
+    // .get(userController.getUser)
+    // .patch(userController.updateUser)
+    // .delete(userController.deleteUser);
+
+module.exports = router;
