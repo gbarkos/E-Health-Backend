2581e55eda199d367d5b5fae229c37cc16eeae14
vasberc <vasberc@gmail.com>
2021-10-15 16:29:03 +0300

    error time log

--- utils/appError.js
+++ utils/appError.js
@@ -8,6 +8,7 @@ class AppError extends Error {
         this.statusCode = statusCode;
         this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
         this.isOperational = true;
+        this.timestump = new Date(Date.now()).toLocaleString("el-GR", {timeZone: "Europe/Athens"});
 
         Error.captureStackTrace(this, this.constructor);
 
